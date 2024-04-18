f8da58ffa33ad3b4ff7b2b242358ce37040a6cab
Dhananjay Sakhare <dsagm7>
2024-04-18 12:37:25 +0530

    33 created comment events

--- blog/comments/index.js
+++ blog/comments/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const axios = require("axios");
 const { randomBytes } = require("crypto");
 const cors = require("cors");
 const app = express();
@@ -11,14 +12,23 @@ app.get("/posts/:id/comments", (req, res) => {
   res.send(commentsByPostId[req.params.id] || []);
 });
 
-app.post("/posts/:id/comments", (req, res) => {
+app.post("/posts/:id/comments", async (req, res) => {
   const commentId = randomBytes(4).toString("hex");
 
   const { content } = req.body;
   const comments = commentsByPostId[req.params.id] || [];
   comments.push({ id: commentId, content });
+
   commentsByPostId[req.params.id] = comments;
 
+  await axios.post("http://localhost:4005/events", {
+    type: "CommentCreated",
+    data: {
+      id: commentId,
+      content,
+      postId: req.params.id,
+    },
+  });
   res.status(201).send(comments);
 });
 
