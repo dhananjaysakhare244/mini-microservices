73d5470003cc8a3f1548a01d74152b4069a4a063
Dhananjay Sakhare <dsagm7>
2024-05-04 20:45:05 +0530

    updated comment after status change

--- blog/comments/index.js
+++ blog/comments/index.js
@@ -37,12 +37,12 @@ app.post("/events", async (req, res) => {
   console.log("Event", req.body.type);
   const { type, data } = req.body;
   if (type === "CommentModerated") {
-    const { postId, id, status } = data;
+    const { postId, id, status, content } = data;
     const comments = commentsByPostId[postId];
 
     const comment = comments.find((comment) => comment.id === id);
     comment.status = status;
-    await axios.post("http://locahost:4005", {
+    await axios.post("http://localhost:4005", {
       type: "CommentUpdated",
       data: {
         id,
