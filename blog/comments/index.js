694b50b5bf9b6ba96308ecddbf77c74116553c98
Dhananjay Sakhare <dsagm7>
2024-05-04 20:33:56 +0530

    updating comment content

--- blog/comments/index.js
+++ blog/comments/index.js
@@ -33,8 +33,25 @@ app.post("/posts/:id/comments", async (req, res) => {
   res.status(201).send(comments);
 });
 
-app.post("/events", (req, res) => {
+app.post("/events", async (req, res) => {
   console.log("Event", req.body.type);
+  const { type, data } = req.body;
+  if (type === "CommentModerated") {
+    const { postId, id, status } = data;
+    const comments = commentsByPostId[postId];
+
+    const comment = comments.find((comment) => comment.id === id);
+    comment.status = status;
+    await axios.post("http://locahost:4005", {
+      type: "CommentUpdated",
+      data: {
+        id,
+        status,
+        postId,
+        content,
+      },
+    });
+  }
   res.send({});
 });
 
